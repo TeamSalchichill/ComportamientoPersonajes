71fad09884c0b3f2e7b14f918c840e869b8e1211
Roberto Alegre <Roberalegre12@hotmail.com>
2022-12-03 12:50:24 +0100

    heroe 1 y torre

--- Assets/Scripts/CP_Hero1_Invocador.cs
+++ Assets/Scripts/CP_Hero1_Invocador.cs
@@ -4,13 +4,87 @@ using UnityEngine;
 
 public class CP_Hero1_Invocador : MonoBehaviour
 {
+    StateMachineEngine FSM_Vivo;
+    //StateMachineEngine FSM_Nivel1;
+    
+
+    public int health = 1000;                      //vida del heroe
+    public bool hayEnemigos = false;        //comprobamos si hay enemigos dentro del rango del heroe
+    public int vidaEnemigos = 0;            //comprobamos si la vida de los enemigos a rango en menor de 4000
+    public bool muroVivo = false;           //comprobamos si ya hay un muro colocado
+
+
     void Start()
     {
+        //Crear maquinas de estado
+        FSM_Vivo = new StateMachineEngine(BehaviourEngine.IsNotASubmachine);
+        //FSM_Nivel1 = new StateMachineEngine(BehaviourEngine.IsNotASubmachine);
+
+        //Creacion de estados
+
+        //estados FSM_Vivo
+        State Idle = FSM_Vivo.CreateEntryState("Idle", accionIdle);
+        State Invocar =FSM_Vivo.CreateState("Invocar", accionInvocar);
+        State Atacar =FSM_Vivo.CreateState("Atacar", accionAtacar);
+
+        //estados FSM_Nivel1
+       //State Vivo =  FSM_Nivel1.CreateSubStateMachine("Vivo", FSM_Vivo);
+        //State Muerto = FSM_Nivel1.CreateState("Muerto", accionMorir);
+
         
+        //Crear percepciones
+        //Percepciones FSM_Nivel1
+        //Perception tengo_vida = FSM_Nivel1.CreatePerception<ValuePerception>(() => health <= 0); //comprobamos si la vida del heroe es menor o igual a cero
+
+        //Percepciones FSM_Vivo
+        Perception enemigos_debiles = FSM_Vivo.CreatePerception<ValuePerception>(() => hayEnemigos == true,() => vidaEnemigos < 4000);
+        Perception no_hay_enemigos = FSM_Vivo.CreatePerception<ValuePerception>(() => hayEnemigos == false);
+        Perception debo_invocar = FSM_Vivo.CreatePerception<ValuePerception>(() => hayEnemigos == true, () => vidaEnemigos > 4000, () => muroVivo == false);
+        Perception ya_invoque_y_hay_enemigos = FSM_Vivo.CreatePerception<ValuePerception>(() => hayEnemigos == true, () => muroVivo == true);
+
+
+
+        //Creacion de transiciones
+        //FSM_Nivel1.CreateTransition("Morir", Vivo, tengo_vida, Muerto);
+
+        FSM_Vivo.CreateTransition("aInvocar", Idle, debo_invocar, Invocar);
+        FSM_Vivo.CreateTransition("aInvocar1", Atacar, debo_invocar, Invocar);
+
+        FSM_Vivo.CreateTransition("aAtacar", Idle, enemigos_debiles, Atacar);
+        FSM_Vivo.CreateTransition("aAtacar1", Invocar, ya_invoque_y_hay_enemigos, Atacar);
+
+        FSM_Vivo.CreateTransition("aIdle", Invocar, no_hay_enemigos, Idle);
+        FSM_Vivo.CreateTransition("aIdle1", Atacar, no_hay_enemigos, Idle);
+
+
+
+
     }
 
     void Update()
     {
-        
+        //FSM_Nivel1.Update();
+        FSM_Vivo.Update();
+
+        //aqui van todas las comprobaciones para cambiar los valores de las variables
+        if(health <= 0)
+        {
+            print("muere");
+        }
     }
+
+    void accionIdle()
+    {
+        Debug.Log("estoy en idle");
+        print("hola");
+    }
+    void accionInvocar()
+    {
+        print("invocar");
+    }
+    void accionAtacar()
+    {
+        print("ataco");
+    }
+    
 }
--- Assets/Scripts/CP_Torres.cs
+++ Assets/Scripts/CP_Torres.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class CP_Torres : MonoBehaviour
 {
+    StateMachineEngine FMS_Torres;
+
+    public int health;
+    public bool hayenemigo;
+
     void Start()
     {
-        
+        FMS_Torres = new StateMachineEngine(false);
+
+        //Creación de estados
+        State Ts_atacar = FMS_Torres.CreateState("atacar", T_atacando);
+        State Ts_no_atacar = FMS_Torres.CreateEntryState("no atacar", T_no_atacando);
+        State Ts_morir = FMS_Torres.CreateState("morir", T_muerte);
+
+        //Creación de percepciones
+        Perception Tp_hay_enemigo = FMS_Torres.CreatePerception<ValuePerception>(() => hayenemigo == true);
+        Perception Tp_no_hay_enemigo = FMS_Torres.CreatePerception<ValuePerception>(() => hayenemigo == false);
+        Perception Tp_sin_vida = FMS_Torres.CreatePerception<ValuePerception>(() => health <= 0);
+
+        //Si hay enemigo ataco
+        FMS_Torres.CreateTransition("atacando", Ts_no_atacar, Tp_hay_enemigo, Ts_atacar);
+        //Si no hay enemigo dejo de atacar
+        FMS_Torres.CreateTransition("no atacando", Ts_atacar, Tp_no_hay_enemigo, Ts_no_atacar);
+        //Si no tengo vida atacando muero
+        FMS_Torres.CreateTransition("muriendo atacando", Ts_atacar, Tp_sin_vida, Ts_morir);
+        //Si no tengo vida no atacando muero
+        FMS_Torres.CreateTransition("muriendo no atacando", Ts_no_atacar, Tp_sin_vida, Ts_morir);
     }
 
     void Update()
     {
-        
+        FMS_Torres.Update();
+    }
+
+    void T_atacando()
+    {
+        print("Ataco");
+    }
+
+    void T_no_atacando()
+    {
+        print("No ataco");
+    }
+
+    void T_muerte()
+    {
+        print("Hasta luego");
     }
 }
