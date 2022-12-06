237b6ef7166611ae44092fa76765b1978c840d5c
DavidPS17 <davidps155@gmail.com>
2022-12-06 20:24:35 +0100

    boss2 casiFinal

--- Assets/Scripts/CP_Boss2_Atacante.cs
+++ Assets/Scripts/CP_Boss2_Atacante.cs
@@ -21,27 +21,21 @@ public class CP_Boss2_Atacante : MonoBehaviour
     public int range;
     public int speed;
     public float hitRate;
-    float hitRateTimer;
+    public float hitRateTimer;
     [Space]
     public float furyRate;
-    float furyRateTimer;
+    public float furyRateTimer;
+    public int minFury;
     public float fury;
     public int furyIncrement;
     public int furyIncrementTime;
-    public int minFury;
     [Space]
     public int curation;
 
     [Header("Checks variables")]
-    public bool towerInRangeCheck;
-    public GameObject towerInRange;
-
-    public bool towerInRangeRunCheck;
+    //public GameObject towerInRange;
     public GameObject towerInRangeRun;
-
-    public bool wallInRangeCheck;
     public GameObject wallInRange;
-
     List<GameObject> towersInRange;
 
     void Start()
@@ -53,7 +47,7 @@ public class CP_Boss2_Atacante : MonoBehaviour
         nav.speed = speed;
 
         hitRateTimer = hitRate;
-        //furyRateTimer = furyRate;
+        furyRateTimer = furyRate;
 
         SM_Boss2 = new StateMachineEngine(false);
         US_Boss2 = new UtilitySystemEngine(true);
@@ -68,11 +62,8 @@ public class CP_Boss2_Atacante : MonoBehaviour
         fury += (Time.deltaTime * furyIncrementTime);
 
         towersInRange = new List<GameObject>();
-        towerInRangeCheck = false;
-        towerInRange = null;
-        towerInRangeRunCheck = false;
+        //towerInRange = null;
         towerInRangeRun = null;
-        wallInRangeCheck = false;
         wallInRange = null;
         foreach (GameObject tower in gameManager.towers)
         {
@@ -82,40 +73,40 @@ public class CP_Boss2_Atacante : MonoBehaviour
                 {
                     if (tower.GetComponent<CP_Torres>())
                     {
-                        if (!towerInRangeCheck)
+                        /*
+                        if (!towerInRange)
                         {
-                            towerInRangeCheck = true;
                             towerInRange = tower;
                         }
+                        */
                         towersInRange.Add(tower);
                     }
+                    /*
                     if (tower.GetComponent<Wall>())
                     {
-                        if (!wallInRangeCheck)
+                        if (!wallInRange)
                         {
-                            wallInRangeCheck = true;
                             wallInRange = tower;
                         }
                         towersInRange.Add(tower);
                     }
                     if (tower.GetComponent<CP_Hero1_Invocador>())
                     {
-                        if (!towerInRangeCheck)
+                        if (!towerInRange)
                         {
-                            towerInRangeCheck = true;
                             towerInRange = tower;
                         }
                         towersInRange.Add(tower);
                     }
                     if (tower.GetComponent<CP_Hero2_Healer>())
                     {
-                        if (!towerInRangeCheck)
+                        if (!towerInRange)
                         {
-                            towerInRangeCheck = true;
                             towerInRange = tower;
                         }
                         towersInRange.Add(tower);
                     }
+                    */
                 }
             }
         }
@@ -136,6 +127,33 @@ public class CP_Boss2_Atacante : MonoBehaviour
         State Boss2_Morir = SM_Boss2.CreateState("Morir", B2_Morir);
         State Boss2_Rabia = SM_Boss2.CreateSubStateMachine("Rabia", US_Boss2);
 
+        Perception Boss2_No_Vida = SM_Boss2.CreatePerception<ValuePerception>(() => health <= 0);
+        Perception Boss2_Hay_Torre = SM_Boss2.CreatePerception<ValuePerception>(() => towersInRange.Count > 0);
+        //Perception Boss2_Volver_Atacar = SM_Boss2.CreatePerception<ValuePerception>(() => towersInRange.Count > 0, () => furyRateTimer < furyRate);
+        Perception Boss2_No_Torre = SM_Boss2.CreatePerception<ValuePerception>(() => towersInRange.Count == 0);
+        //Perception Boss2_Hay_Rabia = SM_Boss2.CreatePerception<ValuePerception>(() => towersInRange.Count > 0, () => fury >= minFury, () => furyRateTimer >= furyRate);
+        Perception Boss2_Hay_Rabia = SM_Boss2.CreatePerception<ValuePerception>(() => fury >= minFury);
+        Perception Boss2_No_Rabia = SM_Boss2.CreatePerception<ValuePerception>(() => fury < minFury);
+
+        SM_Boss2.CreateTransition("Avanzar_Atacar", Boss2_Avanzar, Boss2_Hay_Torre, Boss2_Atacar);
+        SM_Boss2.CreateTransition("Atacar_Avanzar", Boss2_Atacar, Boss2_No_Torre, Boss2_Avanzar);
+        SM_Boss2.CreateTransition("Atacar_Atacar", Boss2_Atacar, Boss2_Hay_Torre, Boss2_Atacar);
+
+        SM_Boss2.CreateTransition("Atacar_Morir", Boss2_Atacar, Boss2_No_Vida, Boss2_Morir);
+        SM_Boss2.CreateTransition("Avanzar_Morir", Boss2_Avanzar, Boss2_No_Vida, Boss2_Morir);
+        SM_Boss2.CreateTransition("Rabia_Morir", Boss2_Rabia, Boss2_No_Vida, Boss2_Morir);
+
+        SM_Boss2.CreateTransition("Atacar_Rabia", Boss2_Atacar, Boss2_Hay_Rabia, Boss2_Rabia);
+        SM_Boss2.CreateTransition("Avanzar_Rabia", Boss2_Avanzar, Boss2_Hay_Rabia, Boss2_Rabia);
+        SM_Boss2.CreateTransition("Rabia_Avanzar", Boss2_Rabia, Boss2_No_Rabia, Boss2_Avanzar);
+
+
+
+
+
+
+
+        /*
         Perception Boss2_hay_enemigos = SM_Boss2.CreatePerception<ValuePerception>(() => towersInRange.Count > 0);
         Perception Boss2_no_hay_enemigos = SM_Boss2.CreatePerception<ValuePerception>(() => towersInRange.Count == 0);
         Perception Boss2_sin_vida = SM_Boss2.CreatePerception<ValuePerception>(() => health <= 0);
@@ -164,6 +182,7 @@ public class CP_Boss2_Atacante : MonoBehaviour
 
         SM_Boss2.CreateTransition("AtacarBucle", Boss2_Atacar, Boss2_no_tengo_rabia_y_enemigo, Boss2_Atacar);
         SM_Boss2.CreateTransition("AtacarBucle2", Boss2_Atacar, Boss2_enemigo_rabia_disable, Boss2_Atacar);
+        */
     }
     void CreateUS()
     {
@@ -173,7 +192,7 @@ public class CP_Boss2_Atacante : MonoBehaviour
         Factor Cant_Torres = new LeafVariable(() => towersInRange.Count, 6, 0);
         //CURVAS
         Factor ImportanciaCurarse = new LinearCurve(Cant_Vida, -1, 1);
-        Factor F_Rabia = new LinearCurve(Cant_Torres, -1, 1);
+        Factor F_Rabia = new LinearCurve(Cant_Torres, 1, -1);
         Factor Damage_Pot = new ExpCurve(Cant_Torres);
 
         // Factores Area
@@ -215,20 +234,22 @@ public class CP_Boss2_Atacante : MonoBehaviour
             print("ATACO");
             hitRateTimer = 0;
             fury += furyIncrement;
-            nav.SetDestination(new Vector3(towerInRange.transform.position.x, 0, towerInRange.transform.position.z));
+            nav.SetDestination(new Vector3(towersInRange[0].transform.position.x, 0, towersInRange[0].transform.position.z));
 
-            if (towerInRange.GetComponent<CP_Torres>())
+            if (towersInRange[0].GetComponent<CP_Torres>())
             {
-                towerInRange.GetComponent<CP_Torres>().health -= damage;
+                towersInRange[0].GetComponent<CP_Torres>().health -= damage;
             }
-            if (towerInRange.GetComponent<CP_Hero1_Invocador>())
+            /*
+            if (towersInRange[0].GetComponent<CP_Hero1_Invocador>())
             {
-                towerInRange.GetComponent<CP_Hero1_Invocador>().health -= damage;
+                towersInRange[0].GetComponent<CP_Hero1_Invocador>().health -= damage;
             }
-            if (towerInRange.GetComponent<CP_Hero2_Healer>())
+            if (towersInRange[0].GetComponent<CP_Hero2_Healer>())
             {
-                towerInRange.GetComponent<CP_Hero2_Healer>().health -= damage;
+                towersInRange[0].GetComponent<CP_Hero2_Healer>().health -= damage;
             }
+            */
         }
     }
     void B2_Heal()
@@ -263,26 +284,28 @@ public class CP_Boss2_Atacante : MonoBehaviour
     }
     void B2_Finisher()
     {
-        print("FINIQUITEO");
-        furyRateTimer = 0;
-        fury = 0;
+        if (towersInRange[0])
+            {
+                print("FINIQUITEO");
+                furyRateTimer = 0;
+                fury = 0;
 
-        if (towerInRange)
-        {
-            nav.SetDestination(new Vector3(towerInRange.transform.position.x, 0, towerInRange.transform.position.z));
+                nav.SetDestination(new Vector3(towersInRange[0].transform.position.x, 0, towersInRange[0].transform.position.z));
 
-            if (towerInRange.GetComponent<CP_Torres>())
-            {
-                towerInRange.GetComponent<CP_Torres>().health -= (damage * 5);
+                if (towersInRange[0].GetComponent<CP_Torres>())
+                {
+                    towersInRange[0].GetComponent<CP_Torres>().health -= (damage * 5);
+                }
+                /*
+                if (towersInRange[0].GetComponent<CP_Hero1_Invocador>())
+                {
+                    towersInRange[0].GetComponent<CP_Hero1_Invocador>().health -= (damage * 5);
+                }
+                if (towersInRange[0].GetComponent<CP_Hero2_Healer>())
+                {
+                    towersInRange[0].GetComponent<CP_Hero2_Healer>().health -= (damage * 5);
+                }
+                */
             }
-            if (towerInRange.GetComponent<CP_Hero1_Invocador>())
-            {
-                towerInRange.GetComponent<CP_Hero1_Invocador>().health -= (damage * 5);
-            }
-            if (towerInRange.GetComponent<CP_Hero2_Healer>())
-            {
-                towerInRange.GetComponent<CP_Hero2_Healer>().health -= (damage * 5);
-            }
-        }
     }
 }
